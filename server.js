b795847cc9f7011c295a6fbc2bdf998f9f9ae908
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-09-06 10:24:40 -0700

    added runserver & closeserver

--- server.js
+++ server.js
@@ -1,12 +1,64 @@
+"use strict";
+// importing middlewares
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+// importing modules
+const config = require("./config");
+
+// using middlewares
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(cors());
+// setting global promise as mongoose promise
+mongoose.Promise = global.Promise;
 
-const PORT = process.env.PORT || 3000;
+// RUN & CLOSE SERVER
+let server = undefined;
 
-app.get("/api/*", (req, res) => {
-  res.json({ ok: true });
+function runServer(url) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(
+      url,
+      err => {
+        if (err) reject(err);
+        server = app
+          .listen(config.PORT, () => {
+            console.log(`Your Server is Listening on localhost:${config.PORT}`);
+            resolve();
+          })
+          .on("error", err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      }
+    );
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(
+    () =>
+      new Promise((resolve, reject) => {
+        console.log("Closing Server");
+        server.close(err => {
+          if (err) reject(err);
+          resolve();
+        });
+      })
+  );
+}
+
+if (require.main === module) {
+  runServer(config.DATABASE_URL).catch(err => console.log(err));
+}
+
+// catch all endpoint
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "Page Not Found" });
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-module.exports = { app };
+module.exports = { app, runServer, closeServer };
