5d771bbfd0a93dd89043f9a2fa546ae716994de0
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-09-11 13:14:09 -0700

    added feature and howto pages

--- public/client.js
+++ public/client.js
@@ -356,86 +356,87 @@ function renderReport(result, year) {
 
 $(document).ready(() => {
   // handle when user clicks register
-  //   $("#register").on("click", () => {
-  //     $(".landing-page").hide();
-  //     $(".register-page").show();
-  //     $(".login-page").hide();
-  //   });
+  $("#register").on("click", () => {
+    $(".landing-page").hide();
+    $(".register-page").show();
+    $(".login-page").hide();
+  });
   //handle when user clicks login
-  //   $("#login").on("click", () => {
-  //     $(".landing-page").hide();
-  //     $(".register-page").hide();
-  //     $(".login-page").show();
-  //   });
+  $("#login").on("click", () => {
+    $(".landing-page").hide();
+    $(".register-page").hide();
+    $(".login-page").show();
+  });
   // handle user registration
-  //   $(".register-form").submit(e => {
-  //     e.preventDefault();
-  //     const firstname = $("#registerFirstName").val();
-  //     const lastname = $("#registerLastName").val();
-  //     const username = $("#registerUserName").val();
-  //     const email = $("#registerEmail").val();
-  //     const password = $("#registerPassword").val();
-  //     const confPassword = $("#confirmPassword").val();
-  //     if (firstname == "") alert("Please enter your first name");
-  //     else if (lastname == "") alert("Please enter your last name");
-  //     else if (username == "") alert("Please enter your user name");
-  //     else if (email == "") alert("Please enter your email");
-  //     else if (password == "") alert("Please enter your password");
-  //     else if (confPassword != password) alert("Password do not match");
-  //     else {
-  //       const newUserObj = {
-  //         firstname: firstname,
-  //         lastname: lastname,
-  //         username: username,
-  //         email: email,
-  //         password: password
-  //       };
-  //       $.ajax({
-  //         type: "POST",
-  //         url: "/users/register",
-  //         dataType: "json",
-  //         data: JSON.stringify(newUserObj),
-  //         contentType: "application/json"
-  //       })
-  //         .done(result => {
-  //           $(".register-page").hide();
-  //           $(".headbar").hide();
-  //           $(".main-page").show();
-  //         })
-  //         .fail(error => console.log(error));
-  //     }
-  //   });
+  $(".register-form").submit(e => {
+    e.preventDefault();
+    const firstname = $("#registerFirstName").val();
+    const lastname = $("#registerLastName").val();
+    const username = $("#registerUserName").val();
+    const email = $("#registerEmail").val();
+    const password = $("#registerPassword").val();
+    const confPassword = $("#confirmPassword").val();
+    if (firstname == "") alert("Please enter your first name");
+    else if (lastname == "") alert("Please enter your last name");
+    else if (username == "") alert("Please enter your user name");
+    else if (email == "") alert("Please enter your email");
+    else if (password == "") alert("Please enter your password");
+    else if (confPassword != password) alert("Password do not match");
+    else {
+      const newUserObj = {
+        firstname: firstname,
+        lastname: lastname,
+        username: username,
+        email: email,
+        password: password
+      };
+      $.ajax({
+        type: "POST",
+        url: "/users/register",
+        dataType: "json",
+        data: JSON.stringify(newUserObj),
+        contentType: "application/json"
+      })
+        .done(result => {
+          $(".register-page").hide();
+          $(".headbar").hide();
+          //===WHEN USER LOGGED IN OR SUCCESFULLY REGISTER, WE CALL THIS FUNCTION
+          populateRecentCrisis();
+          $(".main-page").show();
+        })
+        .fail(error => console.log(error));
+    }
+  });
   // handle user login
-  //   $(".login-form").submit(e => {
-  //     e.preventDefault();
-  //     const username = $("#loginUserName").val();
-  //     const password = $("#loginPassword").val();
-  //     console.log(password);
-  //     if (username == "") alert("Please enter your username");
-  //     else if (password == "") alert("Please enter your password");
-  //     else {
-  //       const loginUserObj = { username: username, password: password };
-  //       $.ajax({
-  //         type: "POST",
-  //         url: "/users/login",
-  //         dataType: "json",
-  //         data: JSON.stringify(loginUserObj),
-  //         contentType: "application/json"
-  //       })
-  //         .done(result => {
-  //           $(".login-page").hide();
-  //           $(".headbar").hide();
-  //           $(".main-page").show();
-  //         })
-  //         .fail(error => {
-  //           console.log(error);
-  //           alert("Incorrect username or password");
-  //         });
-  //     }
-  //   });
-
-  //===WHEN USER LOGGED IN OR SUCCESFULLY REGISTER, WE CALL THIS FUNCTION
-  populateRecentCrisis();
+  $(".login-form").submit(e => {
+    e.preventDefault();
+    const username = $("#loginUserName").val();
+    const password = $("#loginPassword").val();
+    console.log(password);
+    if (username == "") alert("Please enter your username");
+    else if (password == "") alert("Please enter your password");
+    else {
+      const loginUserObj = { username: username, password: password };
+      $.ajax({
+        type: "POST",
+        url: "/users/login",
+        dataType: "json",
+        data: JSON.stringify(loginUserObj),
+        contentType: "application/json"
+      })
+        .done(result => {
+          $(".login-page").hide();
+          $(".headbar").hide();
+          //===WHEN USER LOGGED IN OR SUCCESFULLY REGISTER, WE CALL THIS FUNCTION
+          populateRecentCrisis();
+          $(".main-page").show();
+        })
+        .fail(error => {
+          console.log(error);
+          alert("Incorrect username or password");
+        });
+    }
+  });
 
   //====== RECENT CRISIS PAGE HANDLERS ======
   // When User Clicks "RECENT CRISIS LINK"
@@ -579,6 +580,9 @@ $(document).ready(() => {
   $(".my-crisis").on("click", ".cancel-donation-btn", e => {
     // Prevent Bubbling
     e.preventDefault();
+    $("#charityName").val("");
+    $("#donationAmount").val("");
+    $("#confirmationNumber").val("");
     // Show my-crisis-container
     $(".my-crisis-container").show();
     // Hide donation-page
